268706a0fa2efb12403500ff2e710457d68de5a2
Mariia Kravtsova <mariia@quikly.com>
2018-09-18 22:59:53 -0400

    No tab for now

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 $(function() {
     $('.pop-up').hide();
     $('.pop-up').fadeIn(1000);
@@ -7,15 +6,5 @@ $(function() {
         $('#overlay').removeClass('blur-in');
         $('#overlay').addClass('blur-out');
         e.stopPropagation();
-    });
-
-    $('button').click(function() {
-        console.log('swapout');
-
-        var el = $('.achievement-banner'),
-            newone = el.clone(true);
-
-        el.before(newone);
-        $('.achievement-banner:last').remove();
     });
 });
